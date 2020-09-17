2c15549c0975adbada820098d1cad14bb4a0117f
EmberHeartshine <50666308+EmberHeartshine@users.noreply.github.com>
2020-09-17 11:11:29 -0400

    Update to 0.0.3
    Change from div to table format, sorting options, addtion of individual score mode (mode 2). Functionally I believe rh to be complete.

--- RedHeart_dist/config.js
+++ RedHeart_dist/config.js
@@ -4,12 +4,29 @@ It is recommended that you back this file up when upgrading to avoid data loss.
 All variables are declared as globals. This file WILL be visible on your Web server so be careful what you put here!
 */
 
-const dbUrl = "db.json"; // the URL of the database file (note that if your RedHeart instance is served over HTTPS, then dbUrl must also be HTTPS)
-const pageTitle = "Example Config"; // title of the page (pretty self-explanatory)
-const includeBranding = true; // include the RedHeart branding with version number at the end of the title above
-const contestMode = 1; // defines the contest mode: 1 for recruitment mode (i.e. each team member = 1 point for the team; think blood drives or vaccination drives) and 2 for score mode (i.e. each team member can earn any number of "points" that contribute to the group's total; think charity walks or food drives); if mode 2 is used the database will require a "score" field for each team member (This functionality is not implemented yet)
-const headerTeam = "Team Name"; // the header item for team names in summary view
-const headerTeamCount = "Score"; // the header item for team member number in summary view
-const headerMemberName = "Member Name/Handle"; // the header item for a team member's name in team view
-const headerMemberDate = "Date Donated"; // the header item for a team member's addition to the team
-const headerMemberVerify = "Verified By"; // the header item for the verifying member vouching for a team member
+// the URL of the database file (note that if your RedHeart instance is served over HTTPS, then dbUrl must also be HTTPS)
+const dbUrl = "db.json";
+
+// title of the page (pretty self-explanatory)
+const pageTitle = "Example Config";
+
+// include the RedHeart branding with version number at the end of the title above
+const includeBranding = true;
+
+// defines the contest mode: 1 for recruitment mode (i.e. each team member = 1 point for the team; think blood drives or vaccination drives) and 2 for score mode (i.e. each team member can earn any number of "points" that contribute to the group's total; think charity walks or food drives)
+const contestMode = 1;
+
+// the header item for team names in summary view
+const headerTeam = "Team Name";
+
+// the header item for score counting (number of members in mode 1, team score in mode 2)
+const headerTeamScore = "Score";
+
+// the header item for a team member's name in team view
+const headerMemberName = "Member Name/Handle";
+
+// the header item for a team member's addition to the team or when they last donated
+const headerMemberDate = "Date Donated";
+
+// the header item for the verifying member vouching for a team member
+const headerMemberVerify = "Verified By";
--- RedHeart_dist/redHeart.js
+++ RedHeart_dist/redHeart.js
@@ -1,75 +1,90 @@
-const rhVersion = "0.0.2";
+const rhVersion = "0.0.3";
 var jsonObj;
 async function rhInit(){
 	const pageHeader = document.getElementById( "pageHeader" );
 	pageHeader.innerHTML = pageTitle;
-	if ( includeBranding === true ) {
+	if ( includeBranding ) {
 		document.title = pageTitle + " - RedHeart v" + rhVersion;
 	} else {
 		document.title = pageTitle;
 	};
 	await initJson( dbUrl );
 	await initTeams();
+	console.log( "Contest mode " + contestMode + " enabled." );
 	showTeam( "sum" );
 };
 function showTeam( listValue ){
 	const teamList = document.getElementById( "teamList" );
 	const teamTitle = document.getElementById( "teamTitle" );
-	var teamDiv = document.createElement( "div" );
-	teamDiv.id = "teamList";
-	let headerDiv = document.createElement( "div" );
-	let headerDivCol1 = document.createElement ( "div" );
-	let headerDivCol2 = document.createElement ( "div" );
-	headerDiv.classList.add( "divRow" );
-	headerDivCol1.classList.add( "divCol", "divHeader" );
-	headerDivCol2.classList.add( "divCol", "divHeader" );
+	const teamTable = document.createElement( "table" );
+	teamTable.id = "teamList";
+	const headerRow = document.createElement( "tr" );
+	const headerTheFirst = document.createElement( "th" );
+	headerTheFirst.setAttribute( "onclick", "sortTable(0);" );
+	const headerTheSecond = document.createElement( "th" );
+	headerTheSecond.setAttribute( "onclick", "sortTable(1);" );
 	if ( listValue === "sum" ) {
 		teamTitle.innerHTML = "Summary";
-		headerDivCol1.appendChild( document.createTextNode( headerTeam ) );
-		headerDivCol2.appendChild( document.createTextNode( headerTeamCount ) );
-		headerDiv.appendChild( headerDivCol1 );
-		headerDiv.appendChild( headerDivCol2 );
-		teamDiv.appendChild( headerDiv );
+		headerTheFirst.appendChild( document.createTextNode( headerTeam ) );
+		headerTheSecond.appendChild( document.createTextNode( headerTeamScore ) );
+		headerRow.appendChild( headerTheFirst );
+		headerRow.appendChild( headerTheSecond );
+		teamTable.appendChild( headerRow );
 		for ( let i = 0; i < objectLen( jsonObj.teams._teams ); i++ ) {
-			let newDiv = document.createElement( "div" );
-			let nameCol = document.createElement( "div" );
-			let teamCol = document.createElement( "div" );
-			let newContent = document.createTextNode( jsonObj.teams._teams[i].name );
-			let teamCount = document.createTextNode( objectLen( jsonObj.teams._teams[i] ) );
-			newDiv.classList.add( "divRow" );
-			nameCol.classList.add( "divCol" );
-			teamCol.classList.add( "divCol" );
-			nameCol.appendChild( newContent );
-			teamCol.appendChild( teamCount );
-			newDiv.appendChild( nameCol );
-			newDiv.appendChild( teamCol );
-			teamDiv.appendChild( newDiv );
+			let newRow = document.createElement( "tr" );
+			let nameCol = document.createElement( "td" );
+			let teamCol = document.createElement( "td" );
+			let newName = document.createTextNode( jsonObj.teams._teams[i].name );
+			let teamScore = null;
+			if ( contestMode === 1 ) {
+				teamScore = document.createTextNode( objectLen( jsonObj.teams._teams[i].members ) );
+			} else if ( contestMode === 2 ) {
+				let scoreSum = 0;
+				for ( let n = 0; n < objectLen( jsonObj.teams._teams[i].members ); n++ )
+					scoreSum = scoreSum + jsonObj.teams._teams[i].members[n].memberScore;
+				teamScore = document.createTextNode( scoreSum );
+			} else {
+				console.log( "Error: contestMode is not set to a valid integer." );
+				return 1;	
+			};
+			nameCol.appendChild( newName );
+			teamCol.appendChild( teamScore );
+			newRow.appendChild( nameCol );
+			newRow.appendChild( teamCol );
+			teamTable.appendChild( newRow );
 		};
 	} else {
-		let headerDivCol3 = document.createElement ( "div" );
-		headerDivCol3.classList.add( "divCol", "divHeader" );
+		const headerTheThird = document.createElement ( "th" );
+		headerTheThird.setAttribute( "onclick", "sortTable(2);" );
+		const headerTheFourth = document.createElement ( "th" );
+		headerTheFourth.setAttribute( "onclick", "sortTable(3);" );
 		teamTitle.innerHTML = jsonObj.teams._teams[parseInt( listValue )].name;
-		headerDivCol1.appendChild( document.createTextNode( headerMemberName ) );
-		headerDivCol2.appendChild( document.createTextNode( headerMemberDate ) );
-		headerDivCol3.appendChild( document.createTextNode( headerMemberVerify ) );
-		headerDiv.appendChild( headerDivCol1 );
-		headerDiv.appendChild( headerDivCol2 );
-		headerDiv.appendChild( headerDivCol3 );
-		teamDiv.appendChild( headerDiv );
+		headerTheFirst.appendChild( document.createTextNode( headerMemberName ) );
+		headerTheSecond.appendChild( document.createTextNode( headerMemberDate ) );
+		headerTheThird.appendChild( document.createTextNode( headerTeamScore ) );
+		headerTheFourth.appendChild( document.createTextNode( headerMemberVerify ) );
+		headerRow.appendChild( headerTheFirst );
+		headerRow.appendChild( headerTheSecond );
+		headerRow.appendChild( headerTheThird );
+		headerRow.appendChild( headerTheFourth );
+		teamTable.appendChild( headerRow );
 		for ( let i = 0; i < objectLen( jsonObj.teams._teams[parseInt( listValue )].members ); i++ ) {
-			let newDiv = document.createElement( "div" );
-			newDiv.classList.add( "divRow" );
+			let newRow = document.createElement( "tr" );
 			for ( let n = 0; n < objectLen( jsonObj.teams._teams[parseInt( listValue )].members[i] ); n++ ) {
-				let newCol = document.createElement( "div" );
-				let newContent = document.createTextNode( Object.values( jsonObj.teams._teams[parseInt( listValue )].members[i] )[n] );
-				newCol.classList.add( "divCol" );
+				let newCol = document.createElement( "td" );
+				let newContent = null;
+				if ( contestMode === 1 && n === 2 ) {
+					newContent = document.createTextNode( "N/A" );
+				} else {
+					newContent = document.createTextNode( Object.values( jsonObj.teams._teams[parseInt( listValue )].members[i] )[n] );
+				};
 				newCol.appendChild( newContent );
-				newDiv.appendChild( newCol );
-			}
-			teamDiv.appendChild( newDiv );
+				newRow.appendChild( newCol );
+			};
+			teamTable.appendChild( newRow );
 		};
 	};
-	teamList.parentNode.replaceChild( teamDiv, teamList );
+	teamList.parentNode.replaceChild( teamTable, teamList );
 };
 function initTeams(){
 	const teamList = document.getElementById( "teamDrop" );
@@ -87,6 +102,54 @@ function initTeams(){
 };
 function objectLen( obj ){
 	return Object.keys( obj ).length;
+};
+function sortTable( n ){
+	let table, rows, switching, i, x, y, shouldSwitch, dir, switchCount = 0;
+	table = document.getElementById( "teamList" );
+	headerTab = document.getElementsByTagName( "TH" )[n];
+	switching = true;
+	dir = "asc";
+	while ( switching ) {
+		switching = false;
+		rows = table.rows;
+		for ( i = 1; i < ( rows.length - 1 ); i++ ) {
+			shouldSwitch = false;
+			x = rows[i].getElementsByTagName("TD")[n];
+			y = rows[i + 1].getElementsByTagName("TD")[n];
+			if ( dir === "asc" ) {
+				if ( x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase() ) {
+				shouldSwitch = true;
+				break;
+				};
+			} else if ( dir == "desc" ) {
+				if ( x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase() ) {
+					shouldSwitch = true;
+					break;
+				};
+			};
+		};
+		if ( shouldSwitch ) {
+			rows[i].parentNode.insertBefore( rows[i + 1], rows[i] );
+			switching = true;
+			switchCount++;
+		} else {
+			if ( switchCount === 0 && dir === "asc" ) {
+				dir = "desc";
+				switching = true;
+			};
+		};
+	};
+	for ( i = 0; i < document.getElementsByTagName( "TH" ).length; i++ ) {
+		let changeTab = document.getElementsByTagName( "TH" )[i];
+		if ( changeTab.innerHTML.includes( "\u25B2" ) || changeTab.innerHTML.includes( "\u25BC" ) ) {
+			changeTab.innerHTML = changeTab.innerHTML.substring( 0, changeTab.innerHTML.length - 1 );
+		};
+	};
+	if ( dir === "asc" ) {
+		headerTab.innerHTML = headerTab.innerHTML + "&#x25B2;";
+	} else {
+		headerTab.innerHTML = headerTab.innerHTML + "&#x25BC;";
+	};
 };
 async function initJson( jsonUrl ){
 	const response = await fetch( jsonUrl );
