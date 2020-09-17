1dfcc35be00d5b618128cab81f6276ec10cf9cea
EmberHeartshine <50666308+EmberHeartshine@users.noreply.github.com>
2020-09-16 22:37:59 -0400

    Update config.js

--- RedHeart_dist/config.js
+++ RedHeart_dist/config.js
@@ -7,6 +7,7 @@ All variables are declared as globals. This file WILL be visible on your Web ser
 const dbUrl = "db.json"; // the URL of the database file (note that if your RedHeart instance is served over HTTPS, then dbUrl must also be HTTPS)
 const pageTitle = "Example Config"; // title of the page (pretty self-explanatory)
 const includeBranding = true; // include the RedHeart branding with version number at the end of the title above
+const contestMode = 1; // defines the contest mode: 1 for recruitment mode (i.e. each team member = 1 point for the team; think blood drives or vaccination drives) and 2 for score mode (i.e. each team member can earn any number of "points" that contribute to the group's total; think charity walks or food drives); if mode 2 is used the database will require a "score" field for each team member (This functionality is not implemented yet)
 const headerTeam = "Team Name"; // the header item for team names in summary view
 const headerTeamCount = "Score"; // the header item for team member number in summary view
 const headerMemberName = "Member Name/Handle"; // the header item for a team member's name in team view
